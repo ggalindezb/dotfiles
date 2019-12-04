4b000eccce85aae16a870cdb8bc77dae536f57c7
Gerardo Galindez <ggalindezb@gmail.com>
2019-12-04 17:13:37 -0600

    Update fish config for gpg on Mac

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,9 @@
 # Fish shell configuration file
 #
 # Maintainer:	      Gerardo Galindez
-# Original File:    2012/09/10
+# Original File:    2012/09/10 [bashrc]
 # Created:          2017/04/06
-# Last Updated:     2019/07/10
+# Last Updated:     2019/12/04
 # File Location:    ~/.config/fish/config.fish
 # Sections:
 #    -> General                    [GEN]
@@ -14,19 +14,27 @@
 # """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 # -> General [GEN]
 # """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-rvm default                                 # Load RVM
+# rvm default                                 # Load RVM
+status --is-interactive; and source (rbenv init -|psub)
+direnv hook fish | source
 
 # """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 # -> Aliases [ALI]
 # """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-alias ls='ls -lFGpHh'                       # Proper ls
+alias ls='ls -lFGpHh'                       # Proper ls, Mac
 alias tree='tree -CAh'                      # Color tree
+alias rm='rm -rfi'                          # Confirm rm overkill
+
+# Git
 alias gst='git status -s -b'                # Brief git status
+alias gco='git checkout'                    # Fast switch
 
 # """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 #-> Boot [BOO]
 # """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-#
+set -xg LC_ALL en_US.UTF-8
+set -xg GPG_TTY (tty)
+
 # """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 #-> References [REF]
 # """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
