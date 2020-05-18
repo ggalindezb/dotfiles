dc8c96e0365718fb52cb68435f69e02752c8c312
Gerardo Galindez <ggalindezb@gmail.com>
2020-05-17 20:52:49 -0500

    fish: Disable time check

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,5 +1,5 @@
 function fish_greeting
-  timelimit -T 1 curl 'wttr.in/?0Q' 2> /dev/null  # Weather report
-  fortune | cowsay -f apt                         # Better greet
+  curl 'wttr.in/?0Q' 2> /dev/null  # Weather report
+  fortune | cowsay                 # Better greet
 end
 
