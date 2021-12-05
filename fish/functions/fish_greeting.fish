aa516b185b8fe20948b13c64ab3163f6424b0a41
Gerardo Galindez <ggalindezb@gmail.com>
2021-12-05 10:19:41 -0600

    [fish:zen] Extract zen env, remove nvm

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,5 +1,5 @@
 function fish_greeting
-  curl 'wttr.in/?0Q' 2> /dev/null  # Weather report
-  fortune | cowsay                 # Better greet
+  curl 'wttr.in/?0Q' 2> /dev/null | lolcat # Weather report
+  fortune | cowsay | lolcat                # Better greet
 end
 
